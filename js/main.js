a52f0d449a0040c1e7be259bdf0fa3d1eb5ec7e4
Jefferson Oliveira <jefferoidtex7@gmail.com>
2020-10-20 22:28:03 -0300

    Cria sessão 2 fundo amarelo

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  nav: false,
+  dots: true,
+  items: 1,
+  smartSpeed: 200,
+  animateOut: 'animate__fadeOut',
+  animateIn: 'animate__zoomIn',
+});

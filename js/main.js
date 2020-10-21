f23ef159788aecac2757074d793e2c072ecdca27
Jefferson Oliveira <jefferoidtex7@gmail.com>
2020-10-21 00:12:00 -0300

    Cria filtro cor caixa de som

--- js/main.js
+++ js/main.js
@@ -1,9 +1,41 @@
-$('.owl-carousel').owlCarousel({
-  loop: true,
-  nav: false,
-  dots: true,
-  items: 1,
-  smartSpeed: 200,
-  animateOut: 'animate__fadeOut',
-  animateIn: 'animate__zoomIn',
-});
+window.onload = function(){
+  var write = document.getElementById('button-color-write');
+  var red = document.getElementById('button-color-red');
+  var black = document.getElementById('button-color-black');
+
+  var imageOne = document.getElementById('img-one');
+  var imageTwo = document.getElementById('img-two');
+  var imageThree = document.getElementById('img-three');
+
+
+  write.onclick = function(){
+    imageOne.src = '../assets/images/Bitmap_box_white_frente.png';
+    imageTwo.src = '../assets/images/Bitmap_box_white_lado.png';
+    imageThree.src = '../assets/images/Bitmap_box_white_cima.png';
+  }
+
+  red.onclick = function(){
+    imageOne.src = '../assets/images/Bitmap_box_red.png';
+    imageTwo.src = '../assets/images/Bitmap_box_red_lado.png';
+    imageThree.src = '../assets/images/Bitmap_box_red_cima.png';
+  }
+
+  black.onclick = function(){
+    imageOne.src = '../assets/images/Bitmap_box_black_frente.png';
+    imageTwo.src = '../assets/images/Bitmap_box_black_lado.png';
+    imageThree.src = '../assets/images/Bitmap_box_black_cima.png';
+  }
+
+  $('.owl-carousel').owlCarousel({
+    nav: false,
+    dots: true,
+    items: 1,
+    smartSpeed: 200,
+    animateOut: 'animate__fadeOut',
+    animateIn: 'animate__zoomIn',
+  });
+}
+
+
+
+

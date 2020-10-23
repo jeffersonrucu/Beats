2bd94839dd1fefcc1f818dcc4368f6667bba22f0
Jefferson Oliveira <jefferoidtex7@gmail.com>
2020-10-23 11:39:53 -0300

    Cria barra de busca e tira close do collapse

--- js/main.js
+++ js/main.js
@@ -26,16 +26,30 @@ window.onload = function(){
     imageThree.src = '../assets/images/Bitmap_box_black_cima.png';
   }
 
-  $('.owl-carousel').owlCarousel({
-    nav: false,
-    dots: true,
-    items: 1,
-    smartSpeed: 200,
-    animateOut: 'animate__fadeOut',
-    animateIn: 'animate__zoomIn',
-  });
+  var searchOpen = document.getElementById('search-open');
+  var searchPesq = document.getElementById('search-pesq');
+  var boxNav = document.getElementById('close');
+  var boxSearch = document.getElementById('open');
+
+  searchOpen.onclick = function(){
+    boxNav.style.display = "none";
+    boxSearch.style.display = "flex";
+  }
+
+  searchPesq.onclick = function(){
+    boxNav.style.display = "flex";
+    boxSearch.style.display = "none";
+  }
 }
 
+$('.owl-carousel').owlCarousel({
+  nav: false,
+  dots: true,
+  items: 1,
+  smartSpeed: 200,
+  animateOut: 'animate__fadeOut',
+  animateIn: 'animate__zoomIn',
+});
 
 
 
